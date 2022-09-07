793d83b637edfb0fd66938aa437fa06737eb8422
Michael Youssef <michael_youssef@brown.edu>
2022-09-06 21:14:10 -0400

    added stencil

---
+++ sketchbook/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex flex-row grid grid-cols-5">
+      <div className="h-11/12 col-span-1 border-solid border-2 border-black m-2">
+      </div>
+      <div className="h-11/12 col-span-4 border-solid border-2 border-black m-2">
+      </div>
+    </div>
+  );
+}
+
+export default App;
